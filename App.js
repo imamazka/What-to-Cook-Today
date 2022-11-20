d5ec5ae7231ad5242890cf73612328684b11561f
aryoshiwicaksono <aryoshi.wicaksono@ui.ac.id>
2022-11-21 01:04:13 +0700

    app.js merge fix

--- App.js
+++ App.js
@@ -1,23 +1,15 @@
-<<<<<<< HEAD
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import IngredientList from './Page/IngredientList';
-import Login from './Page/Login';
 import ShoppingCart from './Page/ShoppingCart';
-
-export default function App() {
-  return (
-    <ShoppingCart />
-  );
-=======
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
 import Login from "./Page/Login";
 import Register from "./Page/Register";
 
 export default function App() {
-  return <Register />;
->>>>>>> c97eba57e3b2ace9d0697256b93b4e63fb6c4bf2
+  return (
+    <Register />,
+    <ShoppingCart />
+  );
 }
 
 /*const styles = StyleSheet.create({
