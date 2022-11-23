b0cd337eb8a6911e3faa4c8ee7c254276732eb22
Agungfir08 <agung.firmansyah@ui.ac.id>
2022-11-23 23:47:33 +0700

    add home page

--- App.js
+++ App.js
@@ -1,15 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import IngredientList from './Page/IngredientList';
-import ShoppingCart from './Page/ShoppingCart';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import IngredientList from "./Page/IngredientList";
+import ShoppingCart from "./Page/ShoppingCart";
 import Login from "./Page/Login";
 import Register from "./Page/Register";
-import FoodPage from './Page/FoodPage';
+import FoodPage from "./Page/FoodPage";
+import Home from "./Page/Home";
 
 export default function App() {
-  return (
-    <Login/>
-  );
+  return <Home />;
 }
 
 /*const styles = StyleSheet.create({
