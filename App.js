661e225c97450bd413378a6ae0095906a1747dfb
imamazka <imam.azka@ui.ac.id>
2022-11-24 05:31:15 +0700

    fix App.js

--- App.js
+++ App.js
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import IngredientList from "./Page/IngredientList";
-import ShoppingCart from "./Page/ShoppingCart";
-import Login from "./Page/Login";
-import Register from "./Page/Register";
-import FoodPage from "./Page/FoodPage";
-import Home from "./Page/Home";
-
-export default function App() {
-  return <Home />;
-=======
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
@@ -21,12 +8,12 @@ import Register from "./Page/Register";
 import FoodPage from './Page/FoodPage';
 import FoodList1 from './Page/FoodList1';
 import FoodList2 from './Page/FoodList2';
+import Home from "./Page/Home";
 
 export default function App() {
   return (
-    <FoodPage />
+    <Home />
   );
->>>>>>> 1ae2fa051929cdbf4e0a62b0d3a246aa1888be94
 }
 
 /*const styles = StyleSheet.create({
